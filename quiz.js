174d367924c2d40116dc805ea06c822a94199421
RushikeshNarkhedePatil <rushikeshnarkhede4@gmail.com>
2021-09-17 11:22:36 +0530

    EasyToLearnCodding

---
+++ quiz.js
@@ -0,0 +1,32 @@
+console.log("Connected");
+function fun(op1,op2)
+{
+    document.writeln("Addition is :".op1.value+op2.value)
+}
+
+function Quiz(op1,op2,op3,op4)
+{
+    document.getElementById(op1.value);
+    document.getElementById(op1.value);
+    document.getElementById(op1.value);
+    document.getElementById(op1.value);
+    if(op1.checked==true)
+    {
+        alert("Wrong answer");
+        alert("Correct Answer is depend on compiler");
+    }
+    else if(op2.checked==true)
+    {
+        alert("Wrong answer");
+        alert("Correct Answer is depend on compiler");
+    }
+    else if(op3.checked==true)
+    {
+        alert("Wrong answer");
+        alert("Correct Answer is depend on compiler");
+    }
+    else if(op4.checked==true)
+    {
+        alert("answer is Correct");
+    }
+}
